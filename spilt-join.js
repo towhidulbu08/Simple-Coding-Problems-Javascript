a0ae735eedafe07fbf3acbdca7e0908327d84010
towhidulbu08 <towhidulislam26121997@gmail.com>
2023-08-04 07:50:04 +0600

    third commit

--- spilt-join.js
+++ spilt-join.js
@@ -1,9 +1,25 @@
-const lyrics='Ami tumi Se. ase ki na dekho to. tahole jao giye bari. tata bye'
-const parts=lyrics.split(' ')
-console.log(parts)
-let parts2=lyrics.split('.')
-console.log(parts2)
-let parts3=lyrics.split('')
-console.log(parts3)
-const startToEnd=lyrics.slice(3,10)
-console.log(startToEnd)
+//1.spilt,2.slice,3.substr,4.substring,5.concat,6.join
+const lyrics='Ami Tumi Jabo Jannate Inshahallah. Jannat Is The Only Succes.Hereafter is Truth.Be Carefull Fear The Almighty Allah.'
+//spilt-->return an array..
+const partsSpilt=lyrics.split(' ')
+//console.log(lyrics.split(''))
+//slice--> return an small section of a string using index
+const partial=lyrics.slice(5,8)
+console.log(partial)
+//substring
+const partial2=lyrics.substring(5,8)
+console.log(partial2)
+//concat.
+
+
+//join.
+const lines=
+[
+    'Ami Tumi Jabo Jannate Inshahallah',
+    ' Jannat Is The Only Succes',
+    'Hereafter is Truth',
+    'Be Carefull Fear The Almighty Allah',
+    ''
+  ]
+  const stringJoin=lines.join('. ')
+  console.log(stringJoin)
